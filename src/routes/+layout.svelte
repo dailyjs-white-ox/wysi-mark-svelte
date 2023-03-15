bed945b0efc6bfb6bb26b7198521fe0259cb6f41
KimHeeSang <khsiea@gmail.com>
2023-03-15 23:17:02 +0900

    Add grid layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<div class="parent">
+	<header>
+		<h1>wysi mark</h1>
+	</header>
+	<main>
+		<slot />
+	</main>
+	<footer> footer </footer>
+</div>
+
+<style>
+.parent {
+	display: grid;
+	grid-template-columns: 1fr;
+	grid-template-rows: 60px 1fr 60px;
+	grid-column-gap: 0px;
+	grid-row-gap: 0px;
+	width: 100vw;
+	height: 100vh;
+}
+
+header { grid-area: 1 / 1 / 2 / 2; }
+header h1 { margin: 0;}
+main { 
+	display: grid;
+	grid-template-columns: repeat(4, 1fr);
+	grid-area: 2 / 1 / 3 / 2; 
+}
+footer { grid-area: 3 / 1 / 4 / 2; }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="editor">editor/outline</section>
+<section class="preview">preview/property</section> 
+
+<style>
+.editor {
+	grid-area: 1 / 1 / 2 / 3;
+}
+.preview { 
+	grid-area: 1 / 3 / 2 / 5;
+}
+</style>
