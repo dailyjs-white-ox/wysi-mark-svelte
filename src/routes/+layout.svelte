a8e04ecbf62a799af8b6851901dd70fce38bb35b
KimHeeSang <khsiea@gmail.com>
2023-03-18 17:14:58 +0900

    Apply prettier

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,26 +5,33 @@
 	<main>
 		<slot />
 	</main>
-	<footer> footer </footer>
+	<footer>footer</footer>
 </div>
 
 <style>
-.parent {
-	display: grid;
-	grid-template-columns: 1fr;
-	grid-template-rows: 60px 1fr 60px;
-	grid-column-gap: 0px;
-	grid-row-gap: 0px;
-	width: 100vw;
-	height: 100vh;
-}
+	.parent {
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-template-rows: 60px 1fr 60px;
+		grid-column-gap: 0px;
+		grid-row-gap: 0px;
+		width: 100vw;
+		height: 100vh;
+	}
 
-header { grid-area: 1 / 1 / 2 / 2; }
-header h1 { margin: 0;}
-main { 
-	display: grid;
-	grid-template-columns: repeat(4, 1fr);
-	grid-area: 2 / 1 / 3 / 2; 
-}
-footer { grid-area: 3 / 1 / 4 / 2; }
+	header {
+		grid-area: 1 / 1 / 2 / 2;
+	}
+	header h1 {
+		margin: 0;
+	}
+	main {
+		display: grid;
+		grid-template-rows: 50px 1fr;
+		grid-template-columns: repeat(4, 1fr);
+		grid-area: 2 / 1 / 3 / 2;
+	}
+	footer {
+		grid-area: 3 / 1 / 4 / 2;
+	}
 </style>
