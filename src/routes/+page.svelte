07f082bbd25e26183a6dfb79efe8ad3cb2284f89
KimHeeSang <khsiea@gmail.com>
2023-03-20 22:58:10 +0900

    Add slide page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,64 +1,69 @@
 <script lang="ts">
-	import { unified } from 'unified';
-	import remarkParse from 'remark-parse';
-	import remarkRehype from 'remark-rehype';
-	import rehypeSanitize from 'rehype-sanitize';
-	import rehypeStringify from 'rehype-stringify';
-	import Slide from './Slide.svelte';
+  import { unified } from 'unified';
+  import remarkParse from 'remark-parse';
+  import remarkRehype from 'remark-rehype';
+  import rehypeSanitize from 'rehype-sanitize';
+  import rehypeStringify from 'rehype-stringify';
+  import Slide from './Slide.svelte';
 
-	let source = '';
-	let previewHtml = '';
-	let slideMode = false;
+  let source = '';
+  let previewHtml = '';
+  let slideMode = false;
+  let slides: string[] = [];
 
-	function handleClickShowSlide() {
-		slideMode = true;
-	}
-	function handleClickCloseSlide() {
-		slideMode = false;
-	}
+  function handleClickShowSlide() {
+    slideMode = true;
+  }
+  function handleClickCloseSlide() {
+    slideMode = false;
+  }
 
-	async function generatePreview(source: string) {
-		return await unified()
-			.use(remarkParse)
-			.use(remarkRehype)
-			.use(rehypeSanitize)
-			.use(rehypeStringify)
-			.process(source);
-	}
+  async function generatePreview(source: string) {
+    return await unified()
+      .use(remarkParse)
+      .use(remarkRehype)
+      .use(rehypeSanitize)
+      .use(rehypeStringify)
+      .process(source);
+  }
 
-	$: (async function () {
-		const preview = await generatePreview(source);
-		previewHtml = preview.value.toString();
-	})();
+  $: (async function () {
+    const preview = await generatePreview(source);
+    previewHtml = preview.value.toString();
+    slides = previewHtml
+      .split('<hr>')
+      .map((preview) => preview.trim())
+      .filter((str) => Boolean(str));
+  })();
 </script>
 
 {#if slideMode}
-	<Slide on:clickCloseSlide={handleClickCloseSlide} bind:previewHtml />
+  <Slide on:clickCloseSlide={handleClickCloseSlide} bind:slides />
 {:else}
-	<nav class="navigator">
-		<button on:click={handleClickShowSlide}>Show Slide</button>
-	</nav>
-	<section class="editor">
-		<textarea bind:value={source} />
-	</section>
-	<section class="preview">
-		{@html previewHtml}
-	</section>
+  <nav class="navigator">
+    <button on:click={handleClickShowSlide}>Show Slide</button>
+  </nav>
+  <section class="editor">
+    <textarea bind:value={source} />
+  </section>
+  <section class="preview">
+    {@html previewHtml}
+  </section>
 {/if}
 
 <style>
-	.navigator {
-		grid-area: 1 / 1 / 2 / -1;
-	}
-	.editor {
-		grid-area: 2 / 1 / 3 / 3;
-	}
-	.preview {
-		grid-area: 2 / 3 / 3 / 5;
-	}
-	textarea {
-		height: 90%;
-		width: 95%;
-		margin: 0px 10px;
-	}
+  .navigator {
+    grid-area: 1 / 1 / 2 / -1;
+  }
+  .editor {
+    grid-area: 2 / 1 / 3 / 3;
+  }
+  .preview {
+    grid-area: 2 / 3 / 3 / 5;
+  }
+  textarea {
+    height: 90%;
+    width: 95%;
+    margin: 0px 10px;
+  }
 </style>
--- src/routes/Slide.svelte
+++ src/routes/Slide.svelte
@@ -1,34 +1,38 @@
 <script lang="ts">
-	import { createEventDispatcher } from 'svelte';
-	export let previewHtml: string;
+  import { createEventDispatcher } from 'svelte';
+  export let slides: string[];
 
-	const dispatch = createEventDispatcher();
+  let page = 0;
 
-	function handleClickCloseSlide() {
-		dispatch('clickCloseSlide');
-	}
+  const dispatch = createEventDispatcher();
+
+  function handleClickCloseSlide() {
+    dispatch('clickCloseSlide');
+  }
 </script>
 
 <div class="slide">
-	<nav class="navigator">
-		<button on:click={handleClickCloseSlide}>close slide</button>
-	</nav>
-	<section>
-		{@html previewHtml}
-	</section>
+  <nav class="navigator">
+    <button on:click={handleClickCloseSlide}>close slide</button>
+    <button on:click={() => (page -= 1)} disabled={page === 0}>prev page</button>
+    <button on:click={() => (page += 1)} disabled={page === slides.length - 1}>next page</button>
+  </nav>
+  <section>
+    {@html slides[page]}
+  </section>
 </div>
 
 <style>
-	.navigator {
-		padding: 8px;
-		border-bottom: 1px dashed gray;
-	}
-	.slide {
-		position: absolute;
-		left: 0px;
-		right: 0px;
-		top: 0px;
-		bottom: 0px;
-		background-color: white;
-	}
+  .navigator {
+    padding: 8px;
+    border-bottom: 1px dashed gray;
+  }
+  .slide {
+    position: absolute;
+    left: 0px;
+    right: 0px;
+    top: 0px;
+    bottom: 0px;
+    background-color: white;
+  }
 </style>
